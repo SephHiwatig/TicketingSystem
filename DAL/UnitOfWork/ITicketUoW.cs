385dbb9888342b8f18d773554a5764d6c640feba
Joseph Hiwatig <jhiwatig@itrlab.com>
2020-07-07 09:22:35 -0400

    Added Repository/UnitOfwork

---
+++ DAL/UnitOfWork/ITicketUoW.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.UnitOfWork
+{
+    public interface ITicketUoW
+    {
+        void Save();
+        void Dispose();
+    }
+}
---
+++ DAL/UnitOfWork/TicketUow.cs
@@ -0,0 +1,27 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.UnitOfWork
+{
+    public class TicketUow : IDisposable, ITicketUoW
+    {
+        private ticketContext _context;
+
+        public TicketUow(ticketContext context)
+        {
+            this._context = context;
+        }
+
+        public void Save()
+        {
+            this._context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            this._context.Dispose();
+        }
+    }
+}
